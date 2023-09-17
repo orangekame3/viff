fd2aef2482f48d71ade068261e17c5d1c73656d9
orangekame3 <miya.org.0309@gmail.com>
2023-09-18 00:31:09 +0900

    update

--- cmd/root.go
+++ cmd/root.go
@@ -25,6 +25,8 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/charmbracelet/bubbles/filepicker"
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/gdamore/tcell/v2"
 	"github.com/orangekame3/viff/pkg"
 	"github.com/rivo/tview"
@@ -32,23 +34,114 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type model struct {
+	filepicker   filepicker.Model
+	selectedFile string
+	quitting     bool
+	err          error
+}
+
+func (m *model) Init() tea.Cmd {
+	return m.filepicker.Init()
+}
+
+func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			m.quitting = true
+			return m, tea.Quit
+		}
+	}
+
+	var cmd tea.Cmd
+	m.filepicker, cmd = m.filepicker.Update(msg)
+
+	// Did the user select a file?
+	if didSelect, path := m.filepicker.DidSelectFile(msg); didSelect {
+		// Get the path of the selected file.
+		m.selectedFile = path
+		m.quitting = true
+		return m, tea.Quit
+	}
+
+	return m, cmd
+}
+
+func (m *model) View() string {
+	if m.quitting {
+		return ""
+	}
+	var s string
+	if m.err != nil {
+		s = m.filepicker.Styles.DisabledFile.Render(m.err.Error())
+	} else if m.selectedFile == "" {
+		s = "Pick a file:"
+	} else {
+		s = "Selected file: " + m.filepicker.Styles.Selected.Render(m.selectedFile)
+	}
+	return s + "\n\n" + m.filepicker.View() + "\n"
+}
+
 var rootCmd = &cobra.Command{
 	Use:   "viff",
 	Short: "A tool to display two files side by side in the terminal",
 	Long:  `viff is a CLI tool that takes two file paths as arguments and displays the contents side by side in the terminal.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) < 2 {
-			fmt.Println("requires two file paths as arguments")
+		fp1 := filepicker.New()
+		fp1.AllowedTypes = []string{".txt", ".go", ".md"}
+		fp1.CurrentDirectory, _ = os.Getwd()
+
+		m1 := model{
+			filepicker: fp1,
+		}
+
+		p1 := tea.NewProgram(&m1)
+
+		// 第1段階のfilepickerの実行
+		if _, err := p1.Run(); err != nil {
+			fmt.Printf("Error: %v", err)
 			return
 		}
 
-		oldContent, err := os.ReadFile(args[0])
+		if m1.selectedFile == "" {
+			fmt.Println("No file was selected.")
+			return
+		}
+
+		// 第2段階のfilepickerの設定
+		fp2 := filepicker.New()
+		fp2.AllowedTypes = []string{".txt", ".go", ".md"}
+		fp2.CurrentDirectory, _ = os.Getwd()
+
+		m2 := model{
+			filepicker: fp2,
+		}
+
+		p2 := tea.NewProgram(&m2)
+
+		// 第2段階のfilepickerの実行
+		if _, err := p2.Run(); err != nil {
+			fmt.Printf("Error: %v", err)
+			return
+		}
+
+		if m2.selectedFile == "" {
+			fmt.Println("No file was selected.")
+			return
+		}
+		file1 := m1.selectedFile
+		file2 := m2.selectedFile
+
+		// 選択したファイルを読み込む
+		oldContent, err := os.ReadFile(file1)
 		if err != nil {
 			fmt.Println("failed to read file1: ", err)
 			return
 		}
 
-		newContent, err := os.ReadFile(args[1])
+		newContent, err := os.ReadFile(file2)
 		if err != nil {
 			fmt.Println("failed to read file2: ", err)
 			return
@@ -59,8 +152,8 @@ var rootCmd = &cobra.Command{
 		inlineText := pkg.GenStringForInline(diff)
 
 		// Build View
-		left := pkg.BuildSidePane(oldText, args[0])
-		right := pkg.BuildSidePane(newText, args[1])
+		left := pkg.BuildSidePane(oldText, file1)
+		right := pkg.BuildSidePane(newText, file2)
 		split := pkg.BuildSplitPane(left, right)
 		inline := pkg.BuildInlinePane(inlineText, "inline")
 		pages := pkg.BuildPages(split, inline)
--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/orangekame3/viff
 go 1.21.0
 
 require (
+	github.com/charmbracelet/bubbles v0.16.1
+	github.com/charmbracelet/bubbletea v0.24.2
+	github.com/charmbracelet/lipgloss v0.8.0
 	github.com/gdamore/tcell/v2 v2.6.0
 	github.com/rivo/tview v0.0.0-20230916092115-0ad06c2ea3dd
 	github.com/shibukawa/cdiff v0.1.3
@@ -10,15 +13,25 @@ require (
 )
 
 require (
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/gdamore/encoding v1.0.0 // indirect
 	github.com/gookit/color v1.2.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/rivo/uniseg v0.4.3 // indirect
 	github.com/sergi/go-diff v1.3.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/sys v0.5.0 // indirect
-	golang.org/x/term v0.5.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/term v0.6.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
 )
